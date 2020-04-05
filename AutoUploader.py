c4687a89111780baa456bb3750d26226ea0d365e
Chris <cjperez2017@gmail.com>
2020-04-04 20:08:35 -0500

    autoloaded

--- AutoUploader.py
+++ AutoUploader.py
@@ -4,5 +4,8 @@ import os
 Run this file in the command line and the project will be automatically uploaded to git
 EX: python3 AutoUploader.py
 '''
-
-os.system("git pull && git add .&& git commit -m\"autoloaded\"&&git push")
+pull = input('Do you want to pull before you add your file? (yes/no)')
+if pull.lower() == 'yes' or pull.lower() == 'y':
+    os.system("git pull && git add .&& git commit -m\"autoloaded\"&&git push")
+else:
+    os.system("git add .&& git commit -m\"autoloaded\"&&git push")
