d38b8a1fbdb2ef7195e0f290ac0135b31a8bd2c2
Chris <cjperez2017@gmail.com>
2020-04-04 20:01:50 -0500

    autoloaded

--- Plot.py
+++ Plot.py
@@ -2,12 +2,12 @@ import matplotlib.pyplot as plt
 
 '''
 How to use:
-1. All files you want to find should be stored in the data dir
+1. All files you want to plot should be stored in the data dir
 2. Just the name of the file you wnat is need 
-(ex: if the path of the file is "data/file.txt" you just need "file.txt"
-if the path of the file is "data/date/file.txt" you need "data/file.txt")
-3. The interval should be a number. This number can be an integer or a float
-(it will be converted into a float)
+(ex: if the path of the file is "data/file.csv" you just need "file.csv"
+if the path of the file is "data/date/file.csv" you need "date/file.csv")
+3. The interval is in seconds and should be a number. This number can be an integer or a float
+(it will be converted into a float either way)
 '''
 
 file_name = input('What file would you like to plot?: ')
