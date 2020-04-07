ebb9fbfd803ba702dabecc586991ad177baacf94
Chris <cjperez2017@gmail.com>
2020-04-06 20:12:54 -0500

    autoloaded

--- Plot.py
+++ Plot.py
@@ -9,37 +9,46 @@ if the path of the file is "data/date/file.csv" you need "date/file.csv")
 3. The interval is in seconds and should be a number. This number can be an integer or a float
 (it will be converted into a float either way)
 '''
+def main():
+    plot()
 
-file_name = input('What file would you like to plot?: ')
-interval = input('At what interval (in seconds) are you collecting data?: ')
+def plot():
+    file_name = input('What file would you like to plot?: ')
 
-f = open('data/' + file_name, 'r')
-r = f.read()
+    f = open('data/' + file_name, 'r')
+    r = f.read()
 
-y = r.split(',')
+    y = r.split(',')
+    interval = y[0]
+    y = y[1:]
+    y = [int(x) for x in y]
+    
+    x = []
+    val = 0.0
+    for _ in range(len(y)):
+        x.append(val)
+        val += float(interval)
 
-x = []
-val = 0.0
-for _ in range(len(y)):
-    x.append(val)
-    val += float(interval)
+    plt.plot(x, y)
 
-plt.plot(x, y)
+    x_label = 'Time (' + interval + ' s)'
+    plt.xlabel(x_label)
 
-x_label = 'Time (' + interval + ' s)'
-plt.xlabel(x_label)
+    y_label = 'Analog Value'
+    plt.ylabel(y_label)
 
-y_label = 'Analog Value'
-plt.ylabel(y_label)
+    title = 'Plot of ' + file_name
+    plt.title(title)
 
-title = 'Plot of ' + file_name
-plt.title(title)
+    save = input('Do you want to save the plot? (yes/no): ')
+    if save.lower() == 'yes' or save.lower() == 'y':
+        save_dir = input('Where do you want this data to be stored?: ')
+        plt.savefig(save_dir)
 
-save = input('Do you want to save the plot? (yes/no): ')
-if save.lower() == 'yes' or save.lower() == 'y':
-    save_dir = input('Where do you want this data to be stored?: ')
-    plt.savefig(save_dir)
+    show = input('Do you want to show the plot? (yes/no): ')
+    if show.lower() == 'yes' or show.lower() == 'y':
+        plt.show()
 
-show = input('Do you want to show the plot? (yes/no): ')
-if show.lower() == 'yes' or show.lower() == 'y':
-    plt.show()
+    
+if __name__ == '__main__':
+    main()
--- Sensors/radar_sensor.py
+++ Sensors/radar_sensor.py
@@ -1,15 +1,76 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)          # Note uses BCM instead of BOARD!!!
-GPIO.setup(9, GPIO.IN)         # Read output from PIR motion sensor
 
-while True:
-    reading = GPIO.input(9)
-    if reading is not None:                 # When output from motion sensor is LOW
-        print("Radar Value", reading)
-        time.sleep(.75)
+def main():
+    interval = .75
+    runtime = 20
+    pin_num = 9
+    
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)                              # Note uses BCM instead of BOARD!!!
+    GPIO.setup(pin_num, GPIO.IN)                        # Read output from PIR motion sensor
+    data = collect_data(interval, runtimes, pin_num)    # collects the data from the radar
+
+    if data != -1:
+        save_data_to_csv(data, 'test.csv', '../data/') # saves the data to a file
+
+
+def collect_data(interval, runtime, pin_num):
+    '''
+    params
+    interval in seconds can be an int or float
+    runtime how long you want to collect data (if runtime = -1 it will continue until you C-c out)
+    
+    data will only be collected if interval < runtime, interval > 0, and runtime != -1
+
+    ret
+    data is interval at idx 0 then collected data is at data[1:]
+    if data is not collected retruns -1
+    '''
+    if runtime < interval or interval < 0:
+        print('runtime must be greater than or equal to the runtime')
+        return -1
+    elif runtime == -1:
+        while True:
+            reading = GPIO.input(pin_num)
+            if reading is not None:                 # When output from motion sensor is LOW
+                print("Radar Value", reading)
+                time.sleep(interval)
+            else:
+                print('ERROR: READ NONE')
+                break
     else:
-        print('ERROR: READ NONE')
-        break
+        data = [interval]
+        t = 0
+        while runtime > t:
+            reading = GPIO.input(pin_num)
+            if reading is not None:                 # When output from motion sensor is LOW
+                print("Radar Value", reading)
+                data.append(reading)
+                time.sleep(interval)
+                t += interval
+            else:
+                print('ERROR: READ NONE')
+                break
+
+
+def save_data_to_csv(data, name, path):
+    '''
+    params
+    data should be a list
+    name is a string of what you want the file to be named
+    path is a string where you want the csv file to be saved
+
+    ex: save_data_to_csv([3,2,1], text.csv, ../data/) will save a file named test.csv
+    to the dir the is 1 dir back and then into the data dir and it will contain 3,2,1
+    '''
+    f = open(path+name,'w')
+    data = str(data)[1:-1]
+    f.write(data)
+
+    
+if __name__=='__main__':
+    main()
+
+
