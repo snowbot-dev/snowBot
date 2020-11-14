eefa2fc740986bc8c0a54c4a4db3839e36f4126e
Chris <cjperez2017@gmail.com>
2020-11-14 12:55:08 -0600

    test for digital

---
+++ digital_test.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+print GPIO.input(4)
+
+'''
+sudo apt-get update
+sudo apt-get install python-dev python-pip
+sudo pip install --upgrade distribute
+sudo pip install ipython
+sudo pip install --upgrade RPi.GPIO
+'''
