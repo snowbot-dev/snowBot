1bd541a503e45a14b760926c0728db6319126132
Chris <cjperez2017@gmail.com>
2020-11-22 13:59:25 -0600

    resolved merge conflict

--- digital_test.py
+++ digital_test.py
@@ -3,29 +3,21 @@ import time
 sensor_input = 4
 
 GPIO.setmode(GPIO.BCM)
-<<<<<<< HEAD
-GPIO.setup(sensor_input, GPIO.IN)
-=======
 GPIO.setup(sensor_input, GPIO.IN))
 
 sec = float(input('Interval in seconds: '))
 data = [sec]
->>>>>>> ebf36c36096779c0d1707cbe8b73c646294515f6
+
 try:
    while True:
       if GPIO.input(sensor_input):
          print('detected')
-<<<<<<< HEAD
-         #time.sleep(.5)
-      else:
-         print('NOPE') 
-=======
          data.append(50)
          time.sleep(.5)
       else:
          print('nope')
          data.append(0)
->>>>>>> ebf36c36096779c0d1707cbe8b73c646294515f6
+
 except KeyboardInterrupt:
    f = open('data/digital_test_data.csv', 'w')
    f.write(str(data[1,-1]))
